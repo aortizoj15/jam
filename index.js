f651d699a054e27afb6e03c99d03dcea6a072726
Alexis Ortiz Ojeda <aortizoj15@gmail.com>
2020-12-31 02:23:40 -0800

    Fixed video src

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const port = 3000;
-
+console.log("dirname is: ", __dirname);
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, "assets")));
 
