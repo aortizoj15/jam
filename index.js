34bd71b13177f3042e85fc635eb4e3f9156387ff
Alexis Ortiz Ojeda <aortizoj15@gmail.com>
2020-12-31 01:46:57 -0800

    Added access to assets folder

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const port = 3000;
 
 app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, "assets")));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
